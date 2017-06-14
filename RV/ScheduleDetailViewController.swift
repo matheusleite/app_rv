56a9412281168026a9170cead64c09e8eadc1c11
Matheus Leite <matheus.leite27@gmail.com>
2017-06-14 04:12:49 -0300

    Create new detail classes

---
+++ RV/ScheduleDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ScheduleDetailViewController.swift
+//  RV
+//
+//  Created by Matheus Leite on 14/06/17.
+//  Copyright © 2017 Wavez. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ScheduleDetailViewController: UIViewController {
+
+    let ref = Database.database().reference()
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var durationField: UITextField!
+    @IBOutlet weak var timeField: UITextField!
+    @IBOutlet weak var preacherField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+        self.ref.child("preaches").childByAutoId().setValue(["name": nameField.text!, "duration" : durationField.text!, "time" : timeField.text!, "preacher" : preacherField.text!])
+        
+        let alert = UIAlertController(title: "Pronto!", message: "Sua pregação foi cadastrada.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: { (action) in
+            self.close()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func close () {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+
+}
